1421e346ed44093d56c3301cab2e1c36f9ab7e25
Tjitse Efdé <tjitse@vendic.nl>
2023-02-03 10:48:51 +0100

    Update dependencies

--- src/lottery.ts
+++ src/lottery.ts
@@ -3,176 +3,188 @@ import {Octokit} from '@octokit/rest'
 import {Config} from './config'
 
 export interface Pull {
-  user: {
-    login: string
-  }
-  number: number
-  draft: boolean
+    user: {
+        login: string
+    }
+    number: number
+    draft: boolean
 }
+
 interface Env {
-  repository: string
-  ref: string
+    repository: string
+    ref: string
 }
 
 class Lottery {
-  octokit: Octokit
-  config: Config
-  env: Env
-  pr: Pull | undefined
-
-  constructor({
-    octokit,
-    config,
-    env
-  }: {
     octokit: Octokit
     config: Config
     env: Env
-  }) {
-    this.octokit = octokit
-    this.config = config
-    this.env = {
-      repository: env.repository,
-      ref: env.ref
-    }
-    this.pr = undefined
-  }
+    pr: Pull | undefined
 
-  async run(): Promise<void> {
-    try {
-      const ready = await this.isReadyToReview()
-      if (ready) {
-        const reviewers = await this.selectReviewers()
-        reviewers.length > 0 && (await this.setReviewers(reviewers))
-      }
-    } catch (error) {
-      core.error(error)
-      core.setFailed(error)
-    }
-  }
-
-  async isReadyToReview(): Promise<boolean> {
-    try {
-      const pr = await this.getPR()
-      return !!pr && !pr.draft
-    } catch (error) {
-      core.error(error)
-      core.setFailed(error)
-      return false
-    }
-  }
-
-  async setReviewers(reviewers: string[]): Promise<object> {
-    const ownerAndRepo = this.getOwnerAndRepo()
-    const pr = this.getPRNumber()
-
-    return this.octokit.pulls.requestReviewers({
-      ...ownerAndRepo,
-      pull_number: pr, // eslint-disable-line @typescript-eslint/camelcase
-      reviewers: reviewers.filter((r: string | undefined) => !!r)
-    })
-  }
-
-  async selectReviewers(): Promise<string[]> {
-    let selected: string[] = []
-    const author = await this.getPRAuthor()
-
-    try {
-      for (const {
-        reviewers,
-        internal_reviewers: internalReviewers,
-        usernames
-      } of this.config.groups) {
-        const reviewersToRequest =
-          usernames.includes(author) && internalReviewers
-            ? internalReviewers
-            : reviewers
-
-        if (reviewersToRequest) {
-          selected = selected.concat(
-            this.pickRandom(usernames, reviewersToRequest, author)
-          )
+    constructor({
+                    octokit,
+                    config,
+                    env
+                }: {
+        octokit: Octokit
+        config: Config
+        env: Env
+    }) {
+        this.octokit = octokit
+        this.config = config
+        this.env = {
+            repository: env.repository,
+            ref: env.ref
         }
-      }
-    } catch (error) {
-      core.error(error)
-      core.setFailed(error)
+        this.pr = undefined
     }
 
-    return selected
-  }
-
-  pickRandom(items: string[], n: number, ignore: string): string[] {
-    const picks: string[] = []
-
-    const candidates = items.filter(item => item !== ignore)
-
-    while (picks.length < n) {
-      const random = Math.floor(Math.random() * candidates.length)
-      const pick = candidates.splice(random, 1)[0]
-
-      if (!picks.includes(pick)) picks.push(pick)
+    async run(): Promise<void> {
+        try {
+            const ready = await this.isReadyToReview()
+            if (ready) {
+                const reviewers = await this.selectReviewers()
+                reviewers.length > 0 && (await this.setReviewers(reviewers))
+            }
+        } catch (error) {
+            // @ts-ignore
+            core.error(error)
+            // @ts-ignore
+            core.setFailed(error)
+        }
     }
 
-    return picks
-  }
-
-  async getPRAuthor(): Promise<string> {
-    try {
-      const pr = await this.getPR()
-
-      return pr ? pr.user.login : ''
-    } catch (error) {
-      core.error(error)
-      core.setFailed(error)
+    async isReadyToReview(): Promise<boolean> {
+        try {
+            const pr = await this.getPR()
+            return !!pr && !pr.draft
+        } catch (error) {
+            // @ts-ignore
+            core.error(error)
+            // @ts-ignore
+            core.setFailed(error)
+            return false
+        }
     }
 
-    return ''
-  }
+    async setReviewers(reviewers: string[]): Promise<object> {
+        const ownerAndRepo = this.getOwnerAndRepo()
+        const pr = this.getPRNumber()
 
-  getOwnerAndRepo(): {owner: string; repo: string} {
-    const [owner, repo] = this.env.repository.split('/')
-
-    return {owner, repo}
-  }
-
-  getPRNumber(): number {
-    return Number(this.pr?.number)
-  }
-
-  async getPR(): Promise<Pull | undefined> {
-    if (this.pr) return this.pr
-
-    try {
-      const {data} = await this.octokit.pulls.list({
-        ...this.getOwnerAndRepo()
-      })
-
-      this.pr = data.find(({head: {ref}}) => ref === this.env.ref)
-
-      if (!this.pr) {
-        throw new Error(`PR matching ref not found: ${this.env.ref}`)
-      }
-
-      return this.pr
-    } catch (error) {
-      core.error(error)
-      core.setFailed(error)
-
-      return undefined
+        return this.octokit.pulls.requestReviewers({
+            ...ownerAndRepo,
+            pull_number: pr, // eslint-disable-line @typescript-eslint/camelcase
+            reviewers: reviewers.filter((r: string | undefined) => !!r)
+        })
     }
-  }
+
+    async selectReviewers(): Promise<string[]> {
+        let selected: string[] = []
+        const author = await this.getPRAuthor()
+
+        try {
+            for (const {
+                reviewers,
+                internal_reviewers: internalReviewers,
+                usernames
+            } of this.config.groups) {
+                const reviewersToRequest =
+                    usernames.includes(author) && internalReviewers
+                        ? internalReviewers
+                        : reviewers
+
+                if (reviewersToRequest) {
+                    selected = selected.concat(
+                        this.pickRandom(usernames, reviewersToRequest, author)
+                    )
+                }
+            }
+        } catch (error) {
+            // @ts-ignore
+            core.error(error)
+            // @ts-ignore
+            core.setFailed(error)
+        }
+
+        return selected
+    }
+
+    pickRandom(items: string[], n: number, ignore: string): string[] {
+        const picks: string[] = []
+
+        const candidates = items.filter(item => item !== ignore)
+
+        while (picks.length < n) {
+            const random = Math.floor(Math.random() * candidates.length)
+            const pick = candidates.splice(random, 1)[0]
+
+            if (!picks.includes(pick)) picks.push(pick)
+        }
+
+        return picks
+    }
+
+    async getPRAuthor(): Promise<string> {
+        try {
+            const pr = await this.getPR()
+
+            return pr ? pr.user.login : ''
+        } catch (error) {
+            // @ts-ignore
+            core.error(error)
+            // @ts-ignore
+            core.setFailed(error)
+        }
+
+        return ''
+    }
+
+    getOwnerAndRepo(): { owner: string; repo: string } {
+        const [owner, repo] = this.env.repository.split('/')
+
+        return {owner, repo}
+    }
+
+    getPRNumber(): number {
+        return Number(this.pr?.number)
+    }
+
+    async getPR(): Promise<Pull | undefined> {
+        if (this.pr) return this.pr
+
+        try {
+            const {data} = await this.octokit.pulls.list({
+                ...this.getOwnerAndRepo()
+            })
+
+            // @ts-ignore
+            this.pr = data.find(({head: {ref}}) => ref === this.env.ref)
+
+            if (!this.pr) {
+                throw new Error(`PR matching ref not found: ${this.env.ref}`)
+            }
+
+            return this.pr
+        } catch (error) {
+            // @ts-ignore
+            core.error(error)
+            // @ts-ignore
+            core.setFailed(error)
+
+            return undefined
+        }
+    }
 }
 
 export const runLottery = async (
-  octokit: Octokit,
-  config: Config,
-  env = {
-    repository: process.env.GITHUB_REPOSITORY || '',
-    ref: process.env.GITHUB_HEAD_REF || ''
-  }
+    octokit: Octokit,
+    config: Config,
+    env = {
+        repository: process.env.GITHUB_REPOSITORY || '',
+        ref: process.env.GITHUB_HEAD_REF || ''
+    }
 ): Promise<void> => {
-  const lottery = new Lottery({octokit, config, env})
+    const lottery = new Lottery({octokit, config, env})
 
-  await lottery.run()
+    await lottery.run()
 }
